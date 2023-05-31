ac6f50848fa83f87a102bf99890a33d75e5eaf20
Orya228 <21102150@ittelkom-pwt.ac.id>
2023-05-31 14:09:39 +0700

    pertemuan 7

---
+++ src/com/Anang/PBO/Pertemuan7/Registration.java
@@ -0,0 +1,15 @@
+package com.Anang.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class Registration {
+    private JTextField NamaTextField;
+    private JTextField NimTextField;
+    private JComboBox comboBox1;
+    private JTextField TelpTextField;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+}
