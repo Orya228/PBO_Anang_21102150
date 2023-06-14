76fccf33eeae47badd6edf9724e8275bafc64c96
Orya228 <21102150@ittelkom-pwt.ac.id>
2023-06-14 15:51:17 +0700

    pertemuan 8

--- src/com/Anang/PBO/Pertemuan7/Registration.java
+++ src/com/Anang/PBO/Pertemuan7/Registration.java
@@ -1,15 +1,64 @@
 package com.Anang.PBO.Pertemuan7;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Registration {
+public class Registration extends JFrame{
     private JTextField NamaTextField;
     private JTextField NimTextField;
-    private JComboBox comboBox1;
+    private JComboBox ComboProdi;
     private JTextField TelpTextField;
-    private JRadioButton lakiLakiRadioButton;
-    private JRadioButton perempuanRadioButton;
-    private JTextArea textArea1;
+    private JRadioButton LakiLakiRadioButton;
+    private JRadioButton PerempuanRadioButton;
+    private JTextArea AlamatTextArea;
     private JButton saveButton;
     private JButton clearButton;
+    private JPanel mainPanel;
+    private ButtonGroup JK;
+
+    public Registration(){
+        super("Folmulir registrasi");
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.setSize(800,600);
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NamaTextField.setText("");
+                NimTextField.setText("");
+                TelpTextField.setText("");
+                AlamatTextArea.setText("");
+                ComboProdi.setSelectedIndex(0);
+                JK.clearSelection();
+            }
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = NamaTextField.getText();
+                String NIM = NimTextField.getText();
+                String prodi = (String) ComboProdi.getSelectedItem();
+                String telp = TelpTextField.getText();
+                String alamat = AlamatTextArea.getText();
+                String JK;
+                if(LakiLakiRadioButton.isSelected()){
+                    JK = "Laki-Laki";
+                } else if (PerempuanRadioButton.isSelected()) {
+                    JK = "Perempuan";
+                }else {
+                    JK = "Tidak diketahui";
+                }
+                //Menampilkan msgbox
+                JOptionPane.showMessageDialog
+                        (null,"Data berhasil disimpan!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame mainFrame = new Registration();
+        mainFrame.setVisible(true);
+    }
 }
